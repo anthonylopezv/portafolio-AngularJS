f9043642051acb0bf675fd71d2d559d5dca92c94
Anthony Lopez <anthony.lopez@unmsm.edu.pe>
2017-12-16 12:08:55 -0500

    Servicio de Firebase Listo :D

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,17 +5,31 @@ import { Http } from "@angular/http";
 export class InformacionService {
 
   info:any = {};
+  equipo:any[] = [];
   cargada:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
 
   constructor ( public http:Http ) { 
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
 
-    this.http.get("assets/data/info.pagina.json")
-                .subscribe( data => {
-                    console.log(data.json());
-                    this.cargada = true;
-                    this.info = data.json();
-                } )
+   public carga_info () {
+        this.http.get("assets/data/info.pagina.json")
+        .subscribe( data => {
+            // console.log(data.json());
+            this.cargada = true;
+            this.info = data.json();
+        } )
+   }
 
+   public carga_sobre_nosotros () {
+        this.http.get("https://paginawebangular-c5b64.firebaseio.com/equipo.json")
+        .subscribe( data => {
+            console.log(data.json());
+            this.cargada_sobre_nosotros = true;
+            this.equipo = data.json();
+        } )
    }
 
 }
