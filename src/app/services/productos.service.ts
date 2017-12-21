682f9f9f9167cfc4fc4f627b4d1ababc26821d1c
Anthony Lopez <anthony.lopez@unmsm.edu.pe>
2017-12-20 23:11:43 -0500

    Agregamos firebase al portafolio xd

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -22,7 +22,8 @@ export class ProductosService {
 
           console.log( res.json() );
           this.cargando = false;
-        } );
+          this.productos = res.json();
+        });
 
   }
 
